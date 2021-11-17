42577a9b6f16802cca64e01b26cf05ddbd798eb0
NoXeDev <noyonne2@gmail.com>
2021-11-17 22:57:24 +0100

    refractor code + specify something in README

---
+++ common/nand.js
@@ -0,0 +1,63 @@
+const OTP = require("../crypto/otp")
+const fs = require('fs')
+const crypto = require("crypto")
+const boot9 = require("../crypto/boot9")
+const aes = require('aes-js')
+
+module.exports = class Nand {
+    constructor(buffer)
+    {
+        this.buffer = buffer
+        const tmp = buffer.slice(0x200, 0x200+0xA0)
+        // Check for essential infos
+        if(tmp == '\0' * 0xA0 || tmp == '\xFF' * 0xA0)
+        {
+            throw new Error("Essentials can't be find")
+        }
+    
+        this.essentialBuf = tmp
+
+        this.cid = this._parseEssential('nand_cid')
+        const nandCidhash = crypto.createHash('sha256').update(this.cid).digest()
+        let nandcid = BigInt("0x"+nandCidhash.slice(0x0, 0x10).toString('hex'))
+        this.cryptoCid = nandcid
+
+        if(fs.existsSync('./otp.bin')){
+            this.otp = new OTP(fs.readFileSync('./otp.bin'))
+        }
+        this.otp = new OTP(this._parseEssential('otp'))
+        this.boot9 = new boot9(fs.readFileSync('./boot9.bin'))
+        this.boot9.dumpAndDecryptOTP(this.otp)
+        this.keySlots = this.boot9.setupNandKeyslots()
+
+        let keys3f = this.boot9.extractSlot0x3FNormalKeyAndIV()
+        let NandInfos = `\n
+        \   Nand CID    : 0x${this.cid.toString('hex')}
+        \   OTP key3F   : 0x${keys3f[0].toString('hex')}
+        \   OTP IV 3F   : 0x${keys3f[1].toString('hex')}
+        \   KeySlot0x6  : 0x${this.keySlots.Key0x06.toString('hex')}
+        `
+        console.log(NandInfos)
+    }
+
+    _parseEssential(valName)
+    {
+        for(let i = 0 ; i < this.essentialBuf.length ; i+=0x10)
+        {
+            if(this.essentialBuf.slice(i, i+0x8).toString('utf-8').includes(valName))
+            {
+                let header = this.essentialBuf.slice(i, i+0x10)
+                let offset = header.slice(0x8, 0x8+0x4).readInt16LE()
+                let size = header.slice(0xC, 0xC+0x4).readInt16LE()
+                return this.buffer.slice(0x400 + offset, 0x400 + offset+size)
+            }
+        }
+    }
+
+    _dumpAndDecryptPartition(offset, size, keySlot)
+    {
+        const counter = Buffer.from((this.cryptoCid+BigInt(offset >> 4)).toString(16), 'hex')
+        const aesCtr = new aes.ModeOfOperation.ctr(this.keySlots[keySlot], counter)
+        return aesCtr.decrypt(this.buffer.slice(offset, offset+size))
+    }
+}
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const fs = require("fs")
 const crypto = require("crypto")
-const bootrom = require("./crypto/boot9")
-const aes = require('aes-js')
-const OTP = require('./crypto/otp')
+const CTRNAND = require('./common/nand')
 
 ;
 (async () => {
@@ -26,87 +24,11 @@ const OTP = require('./crypto/otp')
         console.log("Failed to read nand hash, skipping hash check...")
     }
 
-    let nandCid
-    const boot9 = new bootrom(await fs.promises.readFile('./boot9.bin').catch(() => {throw new Error("Can't find boot9.bin (required)")}))
-    if(fs.existsSync('./otp.bin'))
-    {
-        boot9.dumpAndDecryptOTP(new OTP(await fs.promises.readFile('./otp.bin')))
-    }
-    else {
-        const essential = nand.slice(0x200, 0x200+0xA0)
-        if(essential == '\0' * 0xA0 || essential == '\xFF' * 0xA0)
-        {
-            throw new Error("Essentials can't be find")
-        }
+    const NandObj = new CTRNAND(nand)
 
-        for(let i = 0 ; i < essential.length ; i+=0x10)
-        {
-            if(essential.slice(i, i+0x8).toString('utf-8').includes("otp"))
-            {
-                let header = essential.slice(i, i+0x10)
-                let offset = header.slice(0x8, 0x8+0x4).readInt16LE()
-                let size = header.slice(0xC, 0xC+0x4).readInt16LE()
-                boot9.dumpAndDecryptOTP(new OTP(nand.slice(0x400 + offset, 0x400 + offset+size)))
-            }
-
-            if(essential.slice(i, i+0x8).toString('utf-8').includes("nand_cid"))
-            {
-                let header = essential.slice(i, i+0x10)
-                let offset = header.slice(0x8, 0x8+0x4).readInt16LE()
-                let size = header.slice(0xC, 0xC+0x4).readInt16LE()
-                nandCid = nand.slice(0x400 + offset, 0x400 + offset+size)
-            }
-        }
-    }
-
-    const keySlots = boot9.setupNandKeyslots()
-    
-    const firm0Offset = 0x0B130000
-    const firm1Offset = 0x0B530000
-    const CTRFATNandOffset = 0x0B95CA00
-    const firm0 = nand.slice(firm0Offset, firm0Offset+0x00400000)
-    const firm1 = nand.slice(firm1Offset, firm1Offset+0x00400000)
-    const CTRNandFAT = nand.slice(CTRFATNandOffset, CTRFATNandOffset+0x2F3E3600)
-
-    // get Nand cid
-    if(!nandCid){
-        if(fs.existsSync('./nand_cid.mem')){
-            nandCid = await fs.promises.readFile('./nand_cid.mem')
-        } else {
-            throw new Error('Failed to open nand cid (needed)')
-        }
-    }
-
-    let keys3f = boot9.extractSlot0x3FNormalKeyAndIV()
-
-    let NandInfos = `\n
-    \   Nand CID    : 0x${nandCid.toString('hex')}
-    \   OTP key3F   : 0x${keys3f[0].toString('hex')}
-    \   OTP IV 3F   : 0x${keys3f[1].toString('hex')}
-    \   KeySlot0x6  : 0x${keySlots.Key0x06.toString('hex')}
-    `
-    console.log(NandInfos)
-    
-    const nandCidhash = crypto.createHash('sha256').update(nandCid).digest()
-    let nandcid = BigInt("0x"+nandCidhash.slice(0x0, 0x10).toString('hex'))
-
-    const firm0Counter = Buffer.from((nandcid+BigInt(firm0Offset >> 4)).toString(16), 'hex')
-    const firm1Counter = Buffer.from((nandcid+BigInt(firm1Offset >> 4)).toString(16), 'hex')
-
-    const CTRFATNandCounter = Buffer.from((nandcid+BigInt(CTRFATNandOffset >> 4)).toString(16), 'hex')
-
-    const aesCtrFirm0 = new aes.ModeOfOperation.ctr(keySlots.Key0x06, firm0Counter)
-    const decryptedFirm0 = aesCtrFirm0.decrypt(firm0)
-
-    const aesCtrFirm1 = new aes.ModeOfOperation.ctr(keySlots.Key0x06, firm1Counter)
-    const decryptedFirm1 = aesCtrFirm1.decrypt(firm1)
-
-    const aesCtrCTRNANDFAT = new aes.ModeOfOperation.ctr(keySlots.Key0x04, CTRFATNandCounter)
-    const decryptedFATCTRNand = aesCtrCTRNANDFAT.decrypt(CTRNandFAT)
-
-    await fs.promises.writeFile('./firm0.bin', decryptedFirm0)
-    await fs.promises.writeFile('./firm1.bin', decryptedFirm1)
+    await fs.promises.writeFile('./firm0.bin', NandObj._dumpAndDecryptPartition(0x0B130000, 0x00400000, "Key0x06"))
+    await fs.promises.writeFile('./firm1.bin', NandObj._dumpAndDecryptPartition(0x0B530000, 0x00400000, "Key0x06"))
 
     console.log("Decrypt and dump CTRNand Fat filesystem please wait...")
-    await fs.promises.writeFile('CTRNandFAT16.img', decryptedFATCTRNand)
+    await fs.promises.writeFile('CTRNandFAT16.img', NandObj._dumpAndDecryptPartition(0x0B95CA00, 0x2F3E3600, "Key0x04"))
 })()
